57e78a8f89e83114f33b95bf52385266b9e1795b
jdmac020 <john.macgregor@gmail.com>
2019-12-02 18:49:12 -0500

    Convert input to int array

---
+++ AdventOfCode/DayOne.Tests/IntcodeProcessorShould.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Shouldly;
+
+namespace AdventLibrary.Tests
+{
+    public class IntcodeProcessorShould
+    {
+        [Fact]
+        public void CreateArrayFromInput()
+        {
+            var input = "1,9,10,3,2,3,11,0,99,30,40,50";
+            var intProcessor = new IntCodeProcessor(input);
+
+            intProcessor.Code.ShouldBeOfType<int[]>();
+            intProcessor.Code[0].ShouldBe(1);
+            intProcessor.Code[11].ShouldBe(50);
+        }
+    }
+}
---
+++ AdventOfCode/DayOne/IntCodeProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace AdventLibrary
+{
+    public class IntCodeProcessor
+    {
+        public int[] Code { get; }
+
+        public IntCodeProcessor(string input)
+        {
+            var split = input.Split(',');
+            Code = split.Select(item => int.Parse(item)).ToArray();
+        }
+    }
+}
